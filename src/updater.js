96e5f0cd2c56edd8384b1ba1614c306fd377463f
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2021-07-11 21:19:56 +0200

    Added starter.js file downloader to fake updater.js

--- src/updater.js
+++ src/updater.js
@@ -1,2 +1,33 @@
 //This file only exists to ensure compatibility with older start.js when updating automatically
-require("./starter.js").run() //Just start the bot
+var fs = require("fs")
+
+if (!fs.existsSync("./src/starter.js")) { //Get filetostart if it doesn't exist
+    var output = ""
+
+    try {
+        var https = require("https")
+
+        console.log("Pulling starter.js...")
+
+        https.get("https://raw.githubusercontent.com/HerrEurobeat/steam-comment-service-bot/beta-testing/src/starter.js", function (res) {
+            res.setEncoding('utf8');
+
+            res.on('data', function (chunk) {
+                output += chunk 
+            });
+
+            res.on('end', () => {
+                fs.writeFile("./src/starter.js", output, (err) => {
+                    if (err) return console.log(err)
+
+                    require("./starter.js").run() //Just start the bot
+                })
+            }) 
+        });
+    } catch (err) { 
+        console.log('start.js get starter.js function Error: ' + err)
+        process.exit(1)
+    }
+} else {
+    require("./starter.js").run() //Just start the bot
+}
