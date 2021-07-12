c486471ef2b45d5bfe1d984f8b932914a80546de
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2021-07-12 23:45:47 +0200

    Fixed logger function trying to pull ready.js on update

--- src/controller/helpers/logger.js
+++ src/controller/helpers/logger.js
@@ -12,23 +12,28 @@ module.exports.logger = (type, str, nodate, remove) => { //Function that passes 
     var controller   = require("../controller.js")
 
 
-    require("../../starter.js").checkAndGetFile("./src/controller/ready.js", (readyfile) => {
-
-        //Configure my logging library (https://github.com/HerrEurobeat/output-logger#options-1)
-        outputlogger.options({
-            msgstructure: "[date | type] message",
-            paramstructure: ["type", "str", "nodate", "remove"],
-            outputfile: srcdir + "/../output.txt"
-        })
-        
-
-        var string = outputlogger(type, str, nodate, remove)
-
-        if (!nodate) { //startup messages should have nodate enabled -> filter messages with date when bot is not started
-            if (readyfile.readyafter == 0 && !str.toLowerCase().includes("error") && !str.includes('Logging in... Estimated wait time') && !str.includes("What's new:") && remove !== true) { 
-                controller.readyafterlogs.push(string); return; 
-            }
-        }
-
+    //Configure my logging library (https://github.com/HerrEurobeat/output-logger#options-1)
+    outputlogger.options({
+        msgstructure: "[date | type] message",
+        paramstructure: ["type", "str", "nodate", "remove"],
+        outputfile: srcdir + "/../output.txt"
     })
+    
+
+    var string = outputlogger(type, str, nodate, remove)
+
+
+    //Push string to readyafterlogs if desired
+    if (typeof botisloggedin != "undefined" && botisloggedin) { //botislogged in will be undefined when the bot was just started
+
+        require("../../starter.js").checkAndGetFile("./src/controller/ready.js", (readyfile) => {
+
+            if (!nodate) { //startup messages should have nodate enabled -> filter messages with date when bot is not started
+                if (readyfile.readyafter == 0 && !str.toLowerCase().includes("error") && !str.includes('Logging in... Estimated wait time') && !str.includes("What's new:") && remove !== true) { 
+                    controller.readyafterlogs.push(string); return; 
+                }
+            }
+        })
+    }
+
 }
