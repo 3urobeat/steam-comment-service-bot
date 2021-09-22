047803024a8f1c7c7a85a85f8cb812454b2ddff0
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2021-09-22 13:22:18 +0200

    Fixed active comment process detection when preparing update to work with new activecommentprocess object

--- src/updater/helpers/prepareupdate.js
+++ src/updater/helpers/prepareupdate.js
@@ -34,21 +34,41 @@ module.exports.run = (responseSteamID, callback) => {
         }
 
 
-        if (mainfile.activecommentprocess.length != 0) {
+        function filterACPobj() {
+            let objlength = Object.keys(mainfile.activecommentprocess).length //save this before the loop as deleting entries will change this number and lead to the loop finished check never triggering
+
+            Object.keys(mainfile.activecommentprocess).forEach((e, i) => { //loop overr obj to filter invalid/expired entries
+
+                if (mainfile.activecommentprocess[e].status != "active" || Date.now() > mainfile.activecommentprocess[e].until + (config.globalcommentcooldown * 60000)) { //check if status is not active or if entry is finished (realistically the status can't be active and finished but it won't hurt to check both to avoid a possible bug)
+                    delete mainfile.activecommentprocess[e] //remove entry from object
+                }
+    
+                if (i == objlength - 1) {
+                    if (Object.keys(mainfile.activecommentprocess).length > 0) { //check if obj is still not empty and recursively call this function again 
+
+                        setTimeout(() => { //wait 2.5 sec and check again
+                            filterACPobj();
+                        }, 2500);
+
+                    } else { //if the obj is now empty then lets continue with our update
+                        logger("info", "Active comment process finished. Starting to log off all accounts...", false, true, logger.animation("loading"))
+                        if (responseSteamID) controller.botobject[0].chat.sendFriendMessage(responseSteamID, `/me Active comment process finished. Starting to log off all accounts...`)
+
+                        initiateUpdate();
+                    }
+                }
+            })
+        }
+
+
+        //Check for active comment process. If obj not empty then first sort out all invalid/expired entries.
+        if (Object.keys(mainfile.activecommentprocess).length > 0 && Object.values(mainfile.activecommentprocess).some(a => a["status"] == "active")) { //Only check object if it isn't empty and has at least one comment process with the status active
+
             logger("info", "Waiting for an active comment process to finish...", false, true, logger.animation("waiting"))
             if (responseSteamID) controller.botobject[0].chat.sendFriendMessage(responseSteamID, `/me Waiting for an active comment process to finish...`)
 
-            //check if a comment request is being processed every 2.5 secs
-            var activecommentinterval = setInterval(() => { 
-                if (mainfile.activecommentprocess.length == 0) { //start logging off accounts when no comment request is being processed anymore
-                    clearInterval(activecommentinterval);
+            filterACPobj(); //Note: The comment command has already been blocked by updater.js by setting activeupdate = true
 
-                    logger("info", "Active comment process finished. Starting to log off all accounts...", false, true, logger.animation("loading"))
-                    if (responseSteamID) controller.botobject[0].chat.sendFriendMessage(responseSteamID, `/me Active comment process finished. Starting to log off all accounts...`)
-    
-                    initiateUpdate();
-                }
-            }, 2500);
         } else {
             logger("info", "No active comment processes found. Starting to log off all accounts...", false, true, logger.animation("loading"))
             if (responseSteamID) controller.botobject[0].chat.sendFriendMessage(responseSteamID, `/me No active comment processes found. Starting to log off all accounts...`)
