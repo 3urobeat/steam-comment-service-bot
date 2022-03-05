21d857a916f35e9280b42504011192875abff009
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2022-03-05 14:49:44 +0100

    Rewrote getQuote() helper to fix > 1000 chars error and added lastQuotesSize value to advancedconfig.json

--- src/bot/helpers/getQuote.js
+++ src/bot/helpers/getQuote.js
@@ -4,7 +4,7 @@
  * Created Date: 02.03.2022 16:21:11
  * Author: 3urobeat
  * 
- * Last Modified: 02.03.2022 18:15:30
+ * Last Modified: 05.03.2022 14:46:36
  * Modified By: 3urobeat
  * 
  * Copyright (c) 2022 3urobeat <https://github.com/HerrEurobeat>
@@ -24,22 +24,29 @@
 module.exports.getQuote = (quotesArr, lastQuotes, quoteCallback) => {
 
     var randomstring = arr => arr[Math.floor(Math.random() * arr.length)]; //smol function to get random string from array
-    let selection = randomstring(quotesArr); //get random quote for this iteration
+    var selection;
+    var retry = false;
 
-    if (lastQuotes.length > 4) lastQuotes.splice(0, 1) //remove first element from array if we have more than 4 in it
+    if (lastQuotes.length > advancedconfig.lastQuotesSize - 1) lastQuotes.splice(0, 1) //remove first element from array if we have more than 4 in it
     
-    if (lastQuotes.includes(selection)) {
-        logger("debug", "getQuote(): The selected quote is in lastQuotes array. Picking again...")
+    //retry selection until selection isn't in lastQuotes array
+    do {
+        selection = randomstring(quotesArr); //get a random quote
+
+        //Set retry to true and stop this iteration if selection is in lastQuotes array
+        if (lastQuotes.includes(selection)) {
+            logger("debug", "getQuote(): Selected quote is in lastQuotes array, retrying...")
+            retry = true
+            return;
+        }
         
-        this.getQuote(quotesArr, lastQuotes, (cb) => { 
-            quoteCallback(quotesArr, lastQuotes, cb) //call this function again to get a new quote and pass cb to get callback from another execution back to the first one
-        })
-    } else {
-        if (quotesArr.length > 5) lastQuotes.push(selection) //push this comment to lastquotes array to not get it the next 5 times if the quotes.txt has more than 5 quotes
+        retry = false
 
+        //push this comment to lastquotes array to not get it the next lastQuotesSize times if the quotes.txt has more than lastQuotesSize quotes
+        if (quotesArr.length > advancedconfig.lastQuotesSize) lastQuotes.push(selection)
+
+        //make callback
         logger("debug", "getQuote(): Found quote: " + selection)
-
-        quoteCallback(selection)
-    }
-
+        quoteCallback(selection);
+    } while (retry);
 }
