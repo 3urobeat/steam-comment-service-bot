d23b5f30e4e92b938c5fb091af6dfb42beb6ebc0
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2022-10-08 13:55:28 +0200

    Added basic 2FA flow

--- src/bot/helpers/steamSessionHandler.js
+++ src/bot/helpers/steamSessionHandler.js
@@ -4,7 +4,7 @@
  * Created Date: 06.10.2022 20:02:03
  * Author: 3urobeat
  * 
- * Last Modified: 07.10.2022 18:15:24
+ * Last Modified: 08.10.2022 13:54:55
  * Modified By: 3urobeat
  * 
  * Copyright (c) 2022 3urobeat <https://github.com/HerrEurobeat>
@@ -27,6 +27,59 @@ function skipAccount(loginindex) {
 
     controller.skippedaccounts.push(loginindex);
     login.skippednow.push(loginindex);
+}
+
+
+// Helper function to get 2FA code from user
+function get2FAUserInput(thisbot, loginindex, logOnOptions, callback) {
+
+    // Start timer to subtract it later from readyafter time
+    var steamGuardInputStart = Date.now(); // Measure time to subtract it later from readyafter time
+    
+    // Define different question and timeout for main account as it can't be skipped
+    let question;
+    let timeout;
+    
+    if (loginindex == 0) {
+        question = `[${logOnOptions.accountName}] Steam Guard Code: `;
+        timeout = 0;
+    } else {
+        question = `[${logOnOptions.accountName}] Steam Guard Code (leave empty and press ENTER to skip account): `;
+        timeout = 90000;
+    }
+
+    // Ask user for code
+    logger.readInput(question, timeout, (text) => {
+        if (!text || text == "") { // No response or manual skip
+            
+            if (text == null) logger("info", "Skipping account because you didn't respond in 1.5 minutes...", true); // No need to check for main acc as timeout is disabled for it
+
+            if (loginindex == 0) { // First account can't be skipped
+                logger("warn", "The first account always has to be logged in!", true)
+
+                setTimeout(() => {
+                    get2FAUserInput(thisbot, loginindex, logOnOptions, (callback)); // Run function again, pass callback along so a deeper level will callback to the top level
+                }, 500);
+            } else { // Skip account if not bot0
+                logger("info", `[${thisbot}] steamGuard input empty, skipping account...`, false, true, logger.animation("loading"))
+                
+                login.accisloggedin = true; // Set to true to log next account in
+                controller.skippedaccounts.push(loginindex);
+                login.skippednow.push(loginindex);
+                
+                callback(null)
+                return;
+            }
+
+        } else { // User entered code
+
+            logger("info", `[${thisbot}] Accepting Steam Guard Code...`, false, true, logger.animation("loading"))
+            callback(text.toString().trim()) // Give code back to node-steam-user
+
+        }
+
+        login.steamGuardInputTimeFunc(Date.now() - steamGuardInputStart) // Measure time and subtract it from readyafter time
+    })
 }
 
 
@@ -65,42 +118,68 @@ module.exports.getRefreshToken = (thisbot, loginindex, logOnOptions) => {
                         }
                     } 
 
+                    // Get 2FA code/prompt confirmation from user, mentioning the correct source
                     switch (res.validActions[0].type) {
-                        case SteamSession.EAuthSessionGuardType.EmailCode: // 2
+                        case SteamSession.EAuthSessionGuardType.EmailCode:          // Type 2
+                            logger("info", `[${thisbot}] Please enter the Steam Guard Code from your email address at ${res.validActions[0].detail}. Skipping automatically in 1.5 minutes if you don't respond...`, true);
+                            get2FAUserInput(thisbot, loginindex, logOnOptions, (code) => {
+                                session.submitSteamGuardCode(code);
+                            })
                             break;
-                        case SteamSession.EAuthSessionGuardType.DeviceCode: // 3
+
+                        case SteamSession.EAuthSessionGuardType.DeviceConfirmation: // Type 4 (more convenient than type 3, both can be active at the same time so we check for this one first)
+                            logger("info", `[${thisbot}] Please confirm this login request in your Steam Mobile App.`, false, false, logger.animation("waiting"));
                             break;
-                        case SteamSession.EAuthSessionGuardType.DeviceConfirmation: // 4
+
+                        case SteamSession.EAuthSessionGuardType.DeviceCode:         // Type 3
+                            logger("info", `[${thisbot}] Please enter the Steam Guard Code from your Steam Mobile App. Skipping automatically in 1.5 minutes if you don't respond...`, true);
+                            get2FAUserInput(thisbot, loginindex, logOnOptions, (code) => {
+                                session.submitSteamGuardCode(code);
+                            })
                             break;
-                        case SteamSession.EAuthSessionGuardType.EmailConfirmation: // 5
+
+                        case SteamSession.EAuthSessionGuardType.EmailConfirmation:  // Type 5
+                            logger("info", `[${thisbot}] Please confirm this login request via the confirmation email sent to you.`, false, false, logger.animation("waiting"));
                             break;
+
                         default: // Dunno what to do with the other types
                             logger("error", `[${thisbot}] Failed to get login session! Unexpected 2FA type ${res.validActions[0].type}! Sorry, I need to skip this account...`);
                             skipAccount(loginindex);
+                            resolve(null);
+                            return;
                     }
                 }
 
             })
             .catch((err) => {
-                if (err) logger("err", `[${thisbot}] Error trying to get SteamSession with credentials: ${err}`);
+                if (err) {
+                    logger("err", `[${thisbot}] Error trying to get SteamSession with credentials: ${err}`); // TODO: retry?
+                    skipAccount(loginindex);
+                    resolve(null);
+                    return;
+                }
             })
 
        
         /* ------------ Events: ------------ */ 
-        session.on("authenticated", () => {
+        session.on("authenticated", () => { // Success
             logger("debug", `[${thisbot}] getRefreshToken(): Login request successful, '${session.accountName}' authenticated. Resolving Promise...`);
 
             resolve(session.refreshToken);
         })
         
-        session.on("timeout", () => {
+        session.on("timeout", () => { // Login attempt took too long, failure
             logger("warn", `[${thisbot}] Login attempt timed out. Skipping account...`);
+            
             skipAccount(loginindex);
             resolve(null);
         })
 
-        session.on("error", (err) => {
+        session.on("error", (err) => { // Failure
             logger("error", `[${thisbot}] Failed to log in! Error: ${err}\nSkipping account...`);
+
+            // TODO: Retry until advancedconfig.maxLogOnRetries?
+
             skipAccount(loginindex);
             resolve(null);
         })
