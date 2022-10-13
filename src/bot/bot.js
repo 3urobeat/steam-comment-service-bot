449798ae2ec84bead272f878be0d1a2b78a6e421
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2022-10-13 14:18:19 +0200

    Added retry logic to handleCredentialsLoginError helper

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -4,7 +4,7 @@
  * Created Date: 09.07.2021 16:26:00
  * Author: 3urobeat
  * 
- * Last Modified: 13.10.2022 14:16:33
+ * Last Modified: 13.10.2022 14:18:09
  * Modified By: 3urobeat
  * 
  * Copyright (c) 2021 3urobeat <https://github.com/HerrEurobeat>
@@ -88,7 +88,7 @@ module.exports.run = (logOnOptions, loginindex) => {
                 login.additionalaccinfo[loginindex].logOnTries++
 
                 // Log login messsage for this account, with mentioning proxies or without
-                if (thisproxy == null) logger("info", `[${thisbot}] Trying to log in without proxy... (Attempt ${login.additionalaccinfo[loginindex].logOnTries}/${advancedconfig.maxLogOnRetries + 1})`, false, true, logger.animation("loading"))
+                if (!thisproxy) logger("info", `[${thisbot}] Trying to log in without proxy... (Attempt ${login.additionalaccinfo[loginindex].logOnTries}/${advancedconfig.maxLogOnRetries + 1})`, false, true, logger.animation("loading"))
                     else logger("info", `[${thisbot}] Trying to log in with proxy ${login.proxyShift - 1}... (Attempt ${login.additionalaccinfo[loginindex].logOnTries}/${advancedconfig.maxLogOnRetries + 1})`, false, true, logger.animation("loading"))
 
                 // Call our steam-session helper to get a valid refresh token for us
