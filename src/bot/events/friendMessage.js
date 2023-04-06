f3b15f77bf610e1aa1b08c9e36cd8f06a1e3e002
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2023-04-06 17:54:40 +0200

    Implemented CommandHandler runCommand()

--- src/bot/events/friendMessage.js
+++ src/bot/events/friendMessage.js
@@ -4,7 +4,7 @@
  * Created Date: 09.07.2021 16:26:00
  * Author: 3urobeat
  *
- * Last Modified: 03.04.2023 13:41:24
+ * Last Modified: 06.04.2023 12:05:25
  * Modified By: 3urobeat
  *
  * Copyright (c) 2021 3urobeat <https://github.com/HerrEurobeat>
@@ -79,200 +79,22 @@ Bot.prototype._attachSteamFriendMessageEvent = function() {
         });
 
 
-        // Get arguments provided with the message
-        let cont = message.slice("!").split(" ");
-        let args = cont.slice(1);
+        // Handle non-prefixed messages
+        if (!message.startsWith("!")) {
+            if (message.toLowerCase() == ":)") return this.sendChatMessage(steamID, ":))"); // Hehe
 
-        // Run the correct command
-        switch(cont[0].toLowerCase()) {
-            case "!h":
-            case "help":
-            case "!help":
-            case "!commands":
-                require("../commands/general.js").help(ownercheck, chatmsg, steamID, this.controller.data.lang);
-                break;
-
-            case "!comment":
-                if (this.controller.data.advancedconfig.disableCommentCmd) return chatmsg(steamID, this.controller.data.lang.botmaintenance);
-                if (!this.controller.info.readyAfter || this.controller.relogQueue.length > 0) return chatmsg(steamID, this.controller.data.lang.botnotready); // Check if bot is not fully started yet and block cmd usage to prevent errors
-
-                this.controller.data.lastCommentDB.findOne({ id: steam64id }, (err, lastcommentdoc) => {
-                    if (!lastcommentdoc) logger("error", "User is missing from database?? How is this possible?! Error maybe: " + err);
-
-                    try { // Catch any unhandled error to be able to remove user from activecommentprocess array
-                        require("../commands/commentprofile.js").run(chatmsg, steamID, args, this.controller.data.lang, null, lastcommentdoc);
-                    } catch (err) {
-                        chatmsg(steamID, "Sorry, a non comment related error occurred while trying to process your request! Please try again later.");
-                        logger("error", "A non comment related error occurred while trying to process a comment request. Aborting request to make sure nothing weird happens.\n        " + err.stack);
-                    }
-                });
-                break;
-
-
-            case "!gcomment":
-            case "!groupcomment":
-                if (!ownercheck) return notOwnerResponse();
-                if (this.controller.data.advancedconfig.disableCommentCmd) return chatmsg(steamID, this.controller.data.lang.botmaintenance);
-                if (!this.controller.info.readyAfter || this.controller.relogQueue.length > 0) return chatmsg(steamID, this.controller.data.lang.botnotready); // Check if bot is not fully started yet and block cmd usage to prevent errors
-
-                this.controller.data.lastCommentDB.findOne({ id: steam64id }, (err, lastcommentdoc) => {
-                    if (!lastcommentdoc) logger("error", "User is missing from database?? How is this possible?! Error maybe: " + err);
-
-                    try { // Catch any unhandled error to be able to remove user from activecommentprocess array
-                        require("../commands/commentgroup.js").run(chatmsg, steamID, args, this.controller.data.lang, null, lastcommentdoc);
-                    } catch (err) {
-                        chatmsg(steamID, "Error while processing group comment request: " + err.stack);
-                        logger("error", "Error while processing group comment request: " + err);
-                    }
-                });
-                break;
-
-            case "!ping":
-                this.commandHandler.ping({ steamID: steamID });
-                break;
-
-            case "!info":
-                require("../commands/general.js").info(steam64id, chatmsg, steamID);
-                break;
-
-            case "!owner":
-                require("../commands/general.js").owner(chatmsg, steamID, this.controller.data.lang);
-                break;
-
-            case "!group":
-                require("../commands/group.js").group(this.user, chatmsg, steamID, this.controller.data.lang);
-                break;
-
-            case "!abort":
-                if (!this.controller.info.readyAfter || this.controller.relogQueue.length > 0) return chatmsg(steamID, this.controller.data.lang.botnotready); // Check if bot is not fully started yet and block cmd usage to prevent errors
-
-                require("../commands/commentmisc.js").abort(chatmsg, steamID, this.controller.data.lang, args, steam64id);
-                break;
-
-            case "!rc":
-            case "!resetcooldown":
-                if (!ownercheck) return notOwnerResponse();
-                if (this.controller.data.advancedconfig.disableCommentCmd) return chatmsg(steamID, this.controller.data.lang.botmaintenance);
-
-                require("../commands/commentmisc.js").resetCooldown(chatmsg, steamID, this.controller.data.lang, args, steam64id);
-                break;
-
-            case "!config":
-            case "!settings":
-                if (!ownercheck) return notOwnerResponse();
-
-                require("../commands/settings.js").run(chatmsg, steamID, this.controller.data.lang, this.index, args);
-                break;
-
-            case "!failed":
-                require("../commands/commentmisc.js").failed(chatmsg, steamID, this.controller.data.lang, args, steam64id);
-                break;
-
-            case "!sessions":
-                if (!ownercheck) return notOwnerResponse();
-
-                require("../commands/commentmisc.js").sessions(chatmsg, steamID, this.controller.data.lang);
-                break;
-
-            case "!mysessions":
-                require("../commands/commentmisc.js").mysessions(chatmsg, steamID, this.controller.data.lang, steam64id);
-                break;
-
-            case "!about": // Please don't change this message as it gives credit to me; the person who put really much of his free time into this project. The bot will still refer to you - the operator of this instance.
-                require("../commands/general.js").about(chatmsg, steamID);
-                break;
-
-            case "!addfriend":
-                if (!ownercheck) return notOwnerResponse();
-                if (!this.controller.info.readyAfter || this.controller.relogQueue.length > 0) return chatmsg(steamID, this.controller.data.lang.botnotready); // Check if bot is not fully started yet and block cmd usage to prevent errors
-
-                require("../commands/friend.js").addFriend(chatmsg, steamID, this.controller.data.lang, args);
-                break;
-
-            case "!unfriend":
-                if (!ownercheck) return notOwnerResponse();
-                if (!this.controller.info.readyAfter || this.controller.relogQueue.length > 0) return chatmsg(steamID, this.controller.data.lang.botnotready); // Check if bot is not fully started yet and block cmd usage to prevent errors
-
-                require("../commands/friend.js").unfriend(chatmsg, steamID, this.controller.data.lang, args);
-                break;
-
-            case "!unfriendall":
-                if (!ownercheck) return notOwnerResponse();
-                if (!this.controller.info.readyAfter || this.controller.relogQueue.length > 0) return chatmsg(steamID, this.controller.data.lang.botnotready); // Check if bot is not fully started yet and block cmd usage to prevent errors
-
-                require("../commands/friend.js").unfriendAll(chatmsg, steamID, this.controller.data.lang, args);
-                break;
-
-            case "!leavegroup":
-                if (!ownercheck) return notOwnerResponse();
-                if (!this.controller.info.readyAfter || this.controller.relogQueue.length > 0) return chatmsg(steamID, this.controller.data.lang.botnotready); // Check if bot is not fully started yet and block cmd usage to prevent errors
-
-                require("../commands/group.js").leaveGroup(chatmsg, steamID, this.controller.data.lang, args);
-                break;
-
-            case "!leaveallgroups":
-                if (!ownercheck) return notOwnerResponse();
-                if (!this.controller.info.readyAfter || this.controller.relogQueue.length > 0) return chatmsg(steamID, this.controller.data.lang.botnotready); // Check if bot is not fully started yet and block cmd usage to prevent errors
-
-                require("../commands/group.js").leaveAllGroups(chatmsg, steamID, this.controller.data.lang, args);
-                break;
-
-            case "!block": // Well it kinda works but unblocking doesn't. The friend relationship enum stays at 6
-                if (!ownercheck) return notOwnerResponse();
-                if (!this.controller.info.readyAfter || this.controller.relogQueue.length > 0) return chatmsg(steamID, this.controller.data.lang.botnotready); // Check if bot is not fully started yet and block cmd usage to prevent errors
-
-                require("../commands/block.js").block(chatmsg, steamID, this.controller.data.lang, args);
-                break;
-
-            case "!unblock":
-                if (!ownercheck) return notOwnerResponse();
-                if (!this.controller.info.readyAfter || this.controller.relogQueue.length > 0) return chatmsg(steamID, this.controller.data.lang.botnotready); // Check if bot is not fully started yet and block cmd usage to prevent errors
-
-                require("../commands/block.js").unblock(chatmsg, steamID, this.controller.data.lang, args);
-                break;
-
-            case "!rs":
-            case "!restart":
-                if (!ownercheck) return notOwnerResponse();
-
-                require("../commands/system.js").restart(chatmsg, steamID, this.controller.data.lang);
-                break;
-
-            case "!stop":
-                if (!ownercheck) return notOwnerResponse();
-
-                require("../commands/system.js").stop(chatmsg, steamID, this.controller.data.lang);
-                break;
-
-            case "!update":
-                if (!ownercheck) return notOwnerResponse();
-                if (!this.controller.info.readyAfter || this.controller.relogQueue.length > 0) return chatmsg(steamID, this.controller.data.lang.botnotready); // Check if bot is not fully started yet and block cmd usage to prevent errors
-
-                require("../commands/system.js").update(chatmsg, steamID, this.controller.data.lang, args);
-                break;
-
-            case "!log":
-            case "!output":
-                if (!ownercheck) return notOwnerResponse();
-
-                require("../commands/system.js").output(chatmsg, steamID);
-                break;
-
-            case "!eval":
-                if (!this.controller.data.advancedconfig.enableevalcmd) return chatmsg(steamID, this.controller.data.lang.evalcmdturnedoff);
-                if (!ownercheck) return notOwnerResponse();
-
-                require("../commands/system.js").eval(chatmsg, steamID, this.controller.data.lang, args, this.user, this.community);
-                break;
-
-            case ":)":
-                chatmsg(steamID, ":))");
-                break;
-
-            default: // Cmd not recognized
-                if (message.startsWith("!")) chatmsg(steamID, this.controller.data.lang.commandnotfound);
-                    else logger("debug", "Chat message is not a command, ignoring message.");
+            logger("debug", "Chat message is not a command, ignoring message.");
+            return;
         }
+
+
+        // Ask command handler to figure out things for us when a message with prefix was sent
+        let cont = message.slice(1).split(" "); // Remove prefix and split
+        let args = cont.slice(1);               // Remove cmd name to only get arguments
+
+        let success = this.controller.commandHandler.runCommand(cont[0].toLowerCase(), args, this.sendChatMessage, this, resInfo);
+
+        if (!success) this.sendChatMessage(steamID, this.controller.data.lang.commandnotfound); // Send cmd not found msg if runCommand() returned false
 
     });
 
--- src/controller/controller.js
+++ src/controller/controller.js
@@ -4,7 +4,7 @@
  * Created Date: 09.07.2021 16:26:00
  * Author: 3urobeat
  *
- * Last Modified: 05.04.2023 01:32:39
+ * Last Modified: 05.04.2023 20:01:20
  * Modified By: 3urobeat
  *
  * Copyright (c) 2021 3urobeat <https://github.com/HerrEurobeat>
@@ -28,7 +28,16 @@ const Controller = function() {
     // Create eventEmitter
     this.events = new EventEmitter();
 
+    /**
+     * The commandHandler object
+     * @type {CommandHandler}
+     */
     this.commandHandler = {};
+
+    /**
+     * The pluginSystem handler
+     * @type {PluginSystem}
+     */
     this.pluginSystem = {};
 
     /* ------------ Store various stuff: ------------ */
