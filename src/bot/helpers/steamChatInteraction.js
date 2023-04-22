edccd70bde4aaefd46fa1aa454f8c1c4998c22e2
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2023-04-22 19:59:48 +0200

    Fix steamChat respondModule not handling chunks correctly

--- src/bot/helpers/steamChatInteraction.js
+++ src/bot/helpers/steamChatInteraction.js
@@ -4,7 +4,7 @@
  * Created Date: 01.04.2023 21:09:00
  * Author: 3urobeat
  *
- * Last Modified: 16.04.2023 19:12:12
+ * Last Modified: 22.04.2023 19:58:39
  * Modified By: 3urobeat
  *
  * Copyright (c) 2023 3urobeat <https://github.com/HerrEurobeat>
@@ -39,31 +39,31 @@ Bot.prototype.sendChatMessage = function(_this, resInfo, txt, retry, part = 0) {
 
     // Log full message if in debug mode, otherwise log cut down version
     if (_this.controller.data.advancedconfig.printDebug) {
-        logger("debug", `[${_this.logPrefix}] Sending message (${txt.length} chars) to ${resInfo.steamID64} (retry: ${retry == true}, part: ${part}): "${thisPart.replace(/\n/g, "\\n")}"`); // Intentionally checking for == true to prevent showing undefined
+        logger("debug", `[${_this.logPrefix}] Sending message (${thisPart.length} chars) to ${resInfo.steamID64} (retry: ${retry == true}, part: ${part}): "${thisPart.replace(/\n/g, "\\n")}"`); // Intentionally checking for == true to prevent showing undefined
     } else {
-        if (txt.length >= 75) logger("info", `[${_this.logPrefix}] Sending message to ${resInfo.steamID64}: "${thisPart.slice(0, 75).replace(/\n/g, "\\n") + "..."}"`);
+        if (thisPart.length >= 75) logger("info", `[${_this.logPrefix}] Sending message to ${resInfo.steamID64}: "${thisPart.slice(0, 75).replace(/\n/g, "\\n") + "..."}"`);
             else logger("info", `[${_this.logPrefix}] Sending message to ${resInfo.steamID64}: "${thisPart.replace(/\n/g, "\\n")}"`);
     }
 
     // Send part and call function again if this wasn't the last one
-    _this.user.chat.sendFriendMessage(resInfo.steamID64, txt, {}, (err) => {
+    _this.user.chat.sendFriendMessage(resInfo.steamID64, thisPart, {}, (err) => {
         if (err) { // Check for error as some chat messages seem to not get send lately
-            logger("warn", `[${_this.logPrefix}] Error trying to send chat message of length ${txt.length} to ${resInfo.steamID64}! ${err}`);
+            logger("warn", `[${_this.logPrefix}] Error trying to send chat message of length ${thisPart.length} to ${resInfo.steamID64}! ${err}`);
 
             // Send the user a fallback message after 5 seconds just to indicate the bot is not down
             //if (!retry) setTimeout(() => _this.sendChatMessage(resInfo.steamID64, "Sorry, it looks like Steam blocked my last message. Please try again in 30 seconds.", true), 5000);
 
             // Retry message in 10 seconds // TODO: Can I start typing to indicate the bot is not down?
             setTimeout(() => {
-                this.sendChatMessage(_this, resInfo, txt, retry, part); // Send the same part again
+                _this.sendChatMessage(_this, resInfo, txt, retry, part); // Send the same part again
             }, 10000);
 
         } else {
 
             // Send next part if there is one left
             if (750 * (part + 1) <= txt.length) {
-                logger("info", `Message longer than 750 chars, sending next part to '${resInfo.steamID64}' in 7.5 seconds...`, false, false, logger.animation("waiting"));
-                setTimeout(() => this.sendChatMessage(_this, resInfo, txt, retry, part + 1), 7500);
+                logger("info", `Message is ${txt.length} chars long, sending next chunk of 750 chars to '${resInfo.steamID64}' in 7.5 seconds...`, false, false, logger.animation("waiting"));
+                setTimeout(() => _this.sendChatMessage(_this, resInfo, txt, retry, part + 1), 7500);
             } else {
                 logger("debug", "Bot sendChatMessage(): All parts of the message have been sent");
             }
