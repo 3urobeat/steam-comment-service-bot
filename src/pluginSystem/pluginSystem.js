72db891a8ef2daa2258167f364d9b071b5c59697
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2023-05-19 10:30:32 +0200

    Reference commonly used objects for easier access

--- src/pluginSystem/pluginSystem.js
+++ src/pluginSystem/pluginSystem.js
@@ -4,7 +4,7 @@
  * Created Date: 19.03.2023 13:34:27
  * Author: 3urobeat
  *
- * Last Modified: 29.03.2023 17:15:26
+ * Last Modified: 19.05.2023 10:29:40
  * Modified By: 3urobeat
  *
  * Copyright (c) 2023 3urobeat <https://github.com/HerrEurobeat>
@@ -15,13 +15,31 @@
  */
 
 
+const CommandHandler = require("../commands/commandHandler.js"); // eslint-disable-line
+const Controller     = require("../controller/controller.js"); // eslint-disable-line
+const Bot            = require("../bot/bot.js"); // eslint-disable-line
+
+
 /**
  * Constructor - The plugin system loads all plugins and provides functions for plugins to hook into
  * @param {Controller} controller Reference to the controller object
  */
 const PluginSystem = function(controller) {
 
+    // References to commonly used objects for easier access
     this.controller = controller;
+    this.data = controller.data;
+    this.bots = controller.bots;
+
+    /**
+     * @type {Bot}
+     */
+    this.main = controller.main;
+
+    /**
+     * @type {CommandHandler}
+     */
+    this.commandHandler = controller.commandHandler;
 
     // Load helper files
     require("./loadPlugins.js");
