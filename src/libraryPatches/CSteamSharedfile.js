29dabaa637d9f412cb4a6414d68a1338bfc14a8d
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2023-05-28 14:00:12 +0200

    Add support for reading numRatings of guides

--- src/libraryPatches/CSteamSharedfile.js
+++ src/libraryPatches/CSteamSharedfile.js
@@ -25,6 +25,7 @@ SteamCommunity.prototype.getSteamSharedfile = function(sid, callback) {
 		uniqueVisitorsCount: null,
 		favoritesCount: null,
 		upvoteCount: null,
+		guideNumRatings: null,
 		isUpvoted: null,
 		isDownvoted: null
 	};
@@ -104,6 +105,12 @@ SteamCommunity.prototype.getSteamSharedfile = function(sid, callback) {
 			if (upvoteCount) {
 				sharedfile.upvoteCount = Number(upvoteCount);
 			}
+
+
+			// Find numRatings if this is a guide as they use a different voting system
+			let numRatings = $(".ratingSection > .numRatings").text().replace(" ratings", "")
+
+			sharedfile.guideNumRatings = Number(numRatings) || null; // Set to null if not a guide or if the guide does not have enough ratings to show a value
 
 
 			// Determine if this account has already voted on this sharedfile
