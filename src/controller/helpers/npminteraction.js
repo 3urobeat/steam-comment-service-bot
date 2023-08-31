26437210f48b70e4be2eca665a1323faa5aa9241
3urobeat <35304405+3urobeat@users.noreply.github.com>
2023-08-31 18:40:09 +0200

    Fix npminteraction helper failing if package.json is missing

--- src/controller/helpers/npminteraction.js
+++ src/controller/helpers/npminteraction.js
@@ -4,7 +4,7 @@
  * Created Date: 09.07.2021 16:26:00
  * Author: 3urobeat
  *
- * Last Modified: 08.07.2023 00:36:35
+ * Last Modified: 31.08.2023 17:47:57
  * Modified By: 3urobeat
  *
  * Copyright (c) 2021 3urobeat <https://github.com/3urobeat>
@@ -24,7 +24,7 @@ const { exec } = require("child_process"); // Wanted to do it with the npm packa
  * @param {function(string, string): void} logger The currently used logger function (real or fake, the caller decides)
  * @param {function(string|null, string|null): void} callback Called with `err` (String) and `stdout` (String) (npm response) parameters on completion
  */
-module.exports.reinstallAll = (logger, callback) => {
+module.exports.reinstallAll = async (logger, callback) => {
     if (!fs.existsSync(srcdir + "/../node_modules")) {
         logger("info", "Creating node_modules folder...");
 
@@ -33,6 +33,10 @@ module.exports.reinstallAll = (logger, callback) => {
         logger("info", "Deleting node_modules folder content...");
     }
 
+    // Check if package.json is missing
+    await require(srcdir + "/starter.js").checkAndGetFile("./package.json", logger, false, false);
+
+    // Delete node_modules folder content
     fs.rm(srcdir + "/../node_modules", { recursive: true }, (err) => {
         if (err) return callback(err, null);
 
