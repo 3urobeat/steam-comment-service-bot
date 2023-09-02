530ceaa071081eb2cba3357d385e8fc4191fd113
3urobeat <35304405+3urobeat@users.noreply.github.com>
2023-09-02 17:07:31 +0200

    Add generateFileStructure script

---
+++ scripts/generateFileStructure.js
@@ -0,0 +1,81 @@
+/*
+ * File: generateFileStructure.js
+ * Project: steam-comment-service-bot
+ * Created Date: 02.09.2023 14:41:54
+ * Author: 3urobeat
+ *
+ * Last Modified: 02.09.2023 17:05:31
+ * Modified By: 3urobeat
+ *
+ * Copyright (c) 2023 3urobeat <https://github.com/3urobeat>
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+/*
+    This script generates a JSON file containing a path and corresponding GitHub URL for every file in the project.
+    This is used to check and recover missing files on startup.
+    The output is copied to "src/data/fileStructure.json".
+*/
+
+
+const fs = require("fs");
+const path = require("path");
+const crypto = require("crypto");
+
+const ignore = [".git", ".github", "node_modules", "backup", "plugins", "docs", "accounts.txt", "output.txt", "comment-service-bot.code-workspace"];
+
+const output = [];
+
+
+/**
+ * Iterates through all files in the project and pushes them to the output
+ * @param {string} src Project root path
+ * @param {boolean} firstCall Set to `true` on first call, will be set to `false` on recursive call
+ */
+function searchFolderRecursiveSync(src, firstCall) {
+    let files = [];
+
+    // Copy files or call function again if dir
+    if (fs.lstatSync(src).isDirectory()) {
+        files = fs.readdirSync(src);
+
+        let targetFolder = path.join("./", src);
+
+        files.forEach(async (file) => {
+            if (ignore.includes(file)) return; // Ignore this file/folder if name is in ignore array
+            let curSource = path.join(src, file);
+
+            // Recursively call this function again if this is a dir
+            if (fs.lstatSync(curSource).isDirectory()) {
+                searchFolderRecursiveSync(curSource, false);
+
+            } else {
+
+                let filepath = targetFolder + "/" + file;
+
+                // Check if the file resides in the project root and prevent path from resulting in .//start.js
+                if (targetFolder == "./") filepath = file;
+
+                // Construct URL and calculate checksum
+                let fileurl  = "https://raw.githubusercontent.com/3urobeat/steam-comment-service-bot/beta-testing/" + filepath;
+                let filesum  = crypto.createHash("md5").update(fs.readFileSync(filepath)).digest("hex");
+
+                // Add file to output array
+                output.push({ "path": filepath, "url": fileurl, "checksum": filesum });
+            }
+        });
+    }
+
+    // Write output when we are finished and not in a deeper recursion level
+    if (firstCall) {
+        fs.writeFileSync("./src/data/fileStructure.json", JSON.stringify({ "files": output }, null, 4));
+
+        console.log(`Done! Found ${output.length} files and wrote them to 'src/data/fileStructure.json'.`);
+    }
+}
+
+searchFolderRecursiveSync("./", true);
