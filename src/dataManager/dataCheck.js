0a9aab300dfe825f99bc79d96655cf078764b5dc
3urobeat <35304405+3urobeat@users.noreply.github.com>
2023-09-13 17:35:41 +0200

    Add unsupported default language data check

--- src/dataManager/dataCheck.js
+++ src/dataManager/dataCheck.js
@@ -4,7 +4,7 @@
  * Created Date: 09.07.2021 16:26:00
  * Author: 3urobeat
  *
- * Last Modified: 05.09.2023 19:05:31
+ * Last Modified: 13.09.2023 17:35:23
  * Modified By: 3urobeat
  *
  * Copyright (c) 2023 3urobeat <https://github.com/3urobeat>
@@ -89,6 +89,10 @@ DataManager.prototype.checkData = function() {
         }
         if (this.config.randomizeAccounts && Object.keys(this.logininfo).length <= 5 && maxCommentsOverall > Object.keys(this.logininfo).length * 2) {
             logWarn("warn", `${logger.colors.fgred}I wouldn't recommend using randomizeAccounts with 5 or less accounts when each account can/has to comment multiple times. The chance of an account getting a cooldown is higher.\n       Please make sure your commentdelay is set adequately to reduce the chance of this happening.`, true);
+        }
+        if (!Object.keys(this.lang).includes(this.config.defaultLanguage.toLowerCase())) {
+            logWarn("warn", `${logger.colors.fgred}You've set an unsupported language as defaultLanguage in your config.json. Please choose one of the following: ${Object.keys(this.lang).join(", ")}.\n       Defaulting to English...`, true);
+            this.config.defaultLanguage = "english";
         }
         if (this.advancedconfig.loginDelay < 500) { // Don't allow a logindelay below 500ms
             logWarn("error", `${logger.colors.fgred}I won't allow a logindelay below 500ms as this will probably get you blocked by Steam nearly instantly. I recommend setting it to 2500.\n        If you are using one proxy per account you might try setting it to 500 (on your own risk!). Aborting...`, true);
