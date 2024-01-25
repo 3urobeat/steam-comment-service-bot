b8e220512324881f1908c78b6f63c36cdb112617
3urobeat <35304405+3urobeat@users.noreply.github.com>
2024-01-25 18:17:45 +0100

    Me, a dummy, forgot to handle the refreshToken event

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -4,10 +4,10 @@
  * Created Date: 2021-07-09 16:26:00
  * Author: 3urobeat
  *
- * Last Modified: 2023-12-27 13:59:28
+ * Last Modified: 2024-01-25 18:12:11
  * Modified By: 3urobeat
  *
- * Copyright (c) 2021 - 2023 3urobeat <https://github.com/3urobeat>
+ * Copyright (c) 2021 - 2024 3urobeat <https://github.com/3urobeat>
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
@@ -133,6 +133,11 @@ const Bot = function(controller, index) {
     this._attachSteamFriendRelationshipEvent();
     this._attachSteamGroupRelationshipEvent();
     this._attachSteamWebSessionEvent();
+
+    this.user.on("refreshToken", (newToken) => { // Emitted when refreshToken is auto-renewed by SteamUser
+        logger("info", `[${this.logPrefix}] SteamUser auto renewed this refresh token, updating database entry...`);
+        this.sessionHandler._saveTokenToStorage(newToken);
+    });
 
 
     // Get new websession as sometimes the this.user would relog after a lost connection but wouldn't get a websession. Read more about cookies & expiration: https://dev.doctormckay.com/topic/365-cookies/
