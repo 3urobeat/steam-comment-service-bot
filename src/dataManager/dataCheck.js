3urobeat/steam-comment-service-bot

commit 92a661cf1e391e149a1236c5b48ffe64596b6b42

feat: Migrate eslint config for v9

src/dataManager/dataCheck.js
@@ -4,7 +4,7 @@
  * Created Date: 2021-07-09 16:26:00
  * Author: 3urobeat
  *
- * Last Modified: 2024-05-01 13:38:35
+ * Last Modified: 2024-05-03 13:00:04
  * Modified By: 3urobeat
  *
  * Copyright (c) 2023 - 2024 3urobeat <https://github.com/3urobeat>
@@ -110,7 +110,7 @@ DataManager.prototype.checkData = function() {
             logWarn("warn", `You've set an invalid value '${this.advancedconfig.onlineStatus}' as 'onlineStatus' in 'advancedconfig.json'! Defaulting to 'Online'...`);
             this.advancedconfig.onlineStatus = "Online";
         }
-        if (!EPersonaState[this.advancedconfig.childAccOnlineStatus] == undefined) { // Explicitly check for undefined because Offline (0) resolves to false
+        if (EPersonaState[this.advancedconfig.childAccOnlineStatus] != undefined) { // Explicitly check for undefined because Offline (0) resolves to false
             logWarn("warn", `You've set an invalid value '${this.advancedconfig.childAccOnlineStatus}' as 'childAccOnlineStatus' in 'advancedconfig.json'! Defaulting to 'Online'...`);
             this.advancedconfig.childAccOnlineStatus = "Online";
         }

eslint.config.mjs
@@ -1,0 +1,90 @@
+/*
+ * File: eslint.config.mjs
+ * Project: steam-comment-service-bot
+ * Created Date: 2024-05-03 12:17:16
+ * Author: 3urobeat
+ *
+ * Last Modified: 2024-05-03 12:58:49
+ * Modified By: 3urobeat
+ *
+ * Copyright (c) 2024 3urobeat <https://github.com/3urobeat>
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+/*
+    3urobeat's EsLint Config. Requires eslint, @eslint/js & eslint-plugin-jsdoc to be installed as devDependencies.
+    https://github.com/3urobeat
+*/
+
+import globals from "globals";
+import jsdoc from "eslint-plugin-jsdoc";
+import js from "@eslint/js";
+
+
+export default [
+    {
+        ignores: ["src/libraryPatches/*"],
+    },
+    js.configs.recommended, // Import recommended eslint rules
+    {
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: "module",
+            globals: {
+                ...globals.commonjs,
+                ...globals.es6,
+                ...globals.node,
+
+                started: true,
+                srcdir: true,
+                botisloggedin: true,
+                logger: true,
+                extdata: true
+            }
+        },
+        plugins: {
+            jsdoc: jsdoc
+        },
+        rules: {
+            "no-var": "error",
+            "no-redeclare": "off",
+            "no-unreachable": "error",
+            "no-unexpected-multiline": "error",
+
+            // Styling
+            "camelcase": "warn",
+            "capitalized-comments": ["warn", "always", { "ignoreConsecutiveComments": true }],
+            "comma-spacing": ["warn", { "before": false, "after": true }],
+            "func-call-spacing": ["error", "never"],
+            "indent": ["error", 4, { "ignoredNodes": ["IfStatement"], "SwitchCase": 1 }], // TODO: This also ignores if statements with wrong indentation but I couldn't get it to only ignore else in if-else one-liner
+            "no-tabs": "error",
+            "no-trailing-spaces": "error",
+            "no-extra-semi": "error",
+            "semi": ["error", "always"],
+            "semi-spacing": "error",
+            "semi-style": ["error", "last"],
+            "quotes": ["error", "double", { "avoidEscape": true }],
+            "spaced-comment": "warn",
+
+            // JsDoc - https://github.com/gajus/eslint-plugin-jsdoc
+            "jsdoc/check-alignment": "warn",
+            "jsdoc/check-indentation": "warn",
+            "jsdoc/check-types": "warn",
+            "jsdoc/informative-docs": "warn",
+            "jsdoc/require-asterisk-prefix": "warn",
+            "jsdoc/require-description": "warn",
+            "jsdoc/require-jsdoc": "warn",
+            "jsdoc/require-param": "warn",
+            "jsdoc/require-param-name": "warn",
+            "jsdoc/valid-types": "warn",
+            // "jsdoc/require-returns": "warn", // Always requires @return, even when function does not return something
+            "jsdoc/require-returns-type": "warn",
+            // "jsdoc/require-returns-check": "warn", // Unnecessary when require-returns above is disabled
+            "jsdoc/require-returns-description": "warn"
+        }
+    }
+];

src/bot/bot.js
@@ -4,7 +4,7 @@
  * Created Date: 2021-07-09 16:26:00
  * Author: 3urobeat
  *
- * Last Modified: 2024-05-02 10:51:07
+ * Last Modified: 2024-05-03 12:46:26
  * Modified By: 3urobeat
  *
  * Copyright (c) 2021 - 2024 3urobeat <https://github.com/3urobeat>
@@ -133,7 +133,7 @@ const Bot = function(controller, index) {
     require("../libraryPatches/CSteamReviews.js");
     require("../libraryPatches/reviews.js");
 
-    if (global.checkm8!="b754jfJNgZWGnzogvl<rsHGTR4e368essegs9<") this.controller.stop(); // eslint-disable-line
+    if (global.checkm8!="b754jfJNgZWGnzogvl<rsHGTR4e368essegs9<") this.controller.stop();
 
 
     // Attach all SteamUser event listeners we need

src/commands/core/comment.js
@@ -4,7 +4,7 @@
  * Created Date: 2021-07-09 16:26:00
  * Author: 3urobeat
  *
- * Last Modified: 2024-03-02 13:48:36
+ * Last Modified: 2024-05-03 13:08:59
  * Modified By: 3urobeat
  *
  * Copyright (c) 2021 - 2024 3urobeat <https://github.com/3urobeat>
@@ -279,7 +279,7 @@ async function comment(commandHandler, resInfo, respond, postComment, commentArg
 
 
     // Comment numberOfComments times using our syncLoop helper
-    syncLoop(activeReqEntry.amount - (activeReqEntry.thisIteration + 1), (loop, i) => { // eslint-disable-line no-unused-vars
+    syncLoop(activeReqEntry.amount - (activeReqEntry.thisIteration + 1), (loop, i) => {
 
         setTimeout(async () => {
 

src/commands/core/system.js
@@ -4,7 +4,7 @@
  * Created Date: 2021-07-09 16:26:00
  * Author: 3urobeat
  *
- * Last Modified: 2024-02-11 17:03:32
+ * Last Modified: 2024-05-03 13:08:52
  * Modified By: 3urobeat
  *
  * Copyright (c) 2021 - 2024 3urobeat <https://github.com/3urobeat>
@@ -233,7 +233,7 @@ module.exports.eval = {
             return;
         }
 
-        const clean = text => { // eslint-disable-line no-case-declarations
+        const clean = text => {
             if (typeof(text) === "string") return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
                 else return text;
         };

src/commands/helpers/getMiscArgs.js
@@ -4,7 +4,7 @@
  * Created Date: 2023-05-28 12:18:49
  * Author: 3urobeat
  *
- * Last Modified: 2024-02-22 17:51:02
+ * Last Modified: 2024-05-03 13:08:06
  * Modified By: 3urobeat
  *
  * Copyright (c) 2023 - 2024 3urobeat <https://github.com/3urobeat>
@@ -60,7 +60,7 @@ module.exports.getMiscArgs = (commandHandler, args, cmd, resInfo, respond) => {
             }
 
             // Process input and check if ID is valid
-            commandHandler.controller.handleSteamIdResolving(args[1], null, async (err, destParam, idType) => { // eslint-disable-line no-unused-vars
+            commandHandler.controller.handleSteamIdResolving(args[1], null, async (err, destParam, idType) => {
                 logger("debug", `CommandHandler getMiscArgs() success. amount: ${amount} | dest: ${destParam}`);
 
                 resolve({

src/commands/helpers/handleCommentSkips.js
@@ -4,10 +4,10 @@
  * Created Date: 2022-02-28 12:22:48
  * Author: 3urobeat
  *
- * Last Modified: 2023-12-27 14:04:37
+ * Last Modified: 2024-05-03 13:08:38
  * Modified By: 3urobeat
  *
- * Copyright (c) 2022 - 2023 3urobeat <https://github.com/3urobeat>
+ * Copyright (c) 2022 - 2024 3urobeat <https://github.com/3urobeat>
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
@@ -15,7 +15,7 @@
  */
 
 
-const Bot            = require("../../bot/bot.js"); // eslint-disable-line
+const Bot            = require("../../bot/bot.js");
 const CommandHandler = require("../commandHandler.js"); // eslint-disable-line
 
 

src/commands/helpers/handleFollowErrors.js
@@ -4,10 +4,10 @@
  * Created Date: 2023-09-24 22:57:21
  * Author: 3urobeat
  *
- * Last Modified: 2023-12-27 14:04:20
+ * Last Modified: 2024-05-03 13:07:57
  * Modified By: 3urobeat
  *
- * Copyright (c) 2023 3urobeat <https://github.com/3urobeat>
+ * Copyright (c) 2023 - 2024 3urobeat <https://github.com/3urobeat>
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
@@ -15,7 +15,7 @@
  */
 
 
-const Bot = require("../../bot/bot.js"); // eslint-disable-line
+const Bot = require("../../bot/bot.js");
 
 
 /**

commit dbc74b83482367a9e10a9bca1f1095513747e900

fix(DataManager): Fix incorrect childAccOnlineStatus check introduced by 92a661c

src/dataManager/dataCheck.js
@@ -4,7 +4,7 @@
  * Created Date: 2021-07-09 16:26:00
  * Author: 3urobeat
  *
- * Last Modified: 2024-05-03 13:00:04
+ * Last Modified: 2024-05-04 11:28:44
  * Modified By: 3urobeat
  *
  * Copyright (c) 2023 - 2024 3urobeat <https://github.com/3urobeat>
@@ -110,7 +110,7 @@ DataManager.prototype.checkData = function() {
             logWarn("warn", `You've set an invalid value '${this.advancedconfig.onlineStatus}' as 'onlineStatus' in 'advancedconfig.json'! Defaulting to 'Online'...`);
             this.advancedconfig.onlineStatus = "Online";
         }
-        if (EPersonaState[this.advancedconfig.childAccOnlineStatus] != undefined) { // Explicitly check for undefined because Offline (0) resolves to false
+        if (EPersonaState[this.advancedconfig.childAccOnlineStatus] == undefined) { // Explicitly check for undefined because Offline (0) resolves to false
             logWarn("warn", `You've set an invalid value '${this.advancedconfig.childAccOnlineStatus}' as 'childAccOnlineStatus' in 'advancedconfig.json'! Defaulting to 'Online'...`);
             this.advancedconfig.childAccOnlineStatus = "Online";
         }