9d8f4aed77234e99e8d528364a85129c413f4d62
3urobeat <35304405+3urobeat@users.noreply.github.com>
2025-01-02 13:24:20 +0100

    feat: Add !manage command which implements manage module #228

--- src/commands/core/system.js
+++ src/commands/core/system.js
@@ -4,10 +4,10 @@
  * Created Date: 2021-07-09 16:26:00
  * Author: 3urobeat
  *
- * Last Modified: 2024-05-03 13:08:52
+ * Last Modified: 2025-01-02 13:23:12
  * Modified By: 3urobeat
  *
- * Copyright (c) 2021 - 2024 3urobeat <https://github.com/3urobeat>
+ * Copyright (c) 2021 - 2025 3urobeat <https://github.com/3urobeat>
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
@@ -269,6 +269,99 @@ module.exports.eval = {
             respond(`Error:\n${clean(err)}`);
             logger("error", `${logger.colors.fgyellow}Eval error:${logger.colors.reset} \n${clean(err)}`, true);                                                                                                                                                                                                                                                                                                                // Hi I'm a comment that serves no purpose
             return;
+        }
+    }
+};
+
+
+const availableManageModes = "addAccount, removeAccount, filterAccounts";
+
+module.exports.manage = {
+    names: ["manage"],
+    description: "Interact with the manage module to administrate the active set of bot accounts",
+    args: [
+        {
+            name: "mode",
+            description: `Available modes: '${availableManageModes}'`,
+            type: "string",
+            isOptional: false,
+            ownersOnly: true
+        },
+        {
+            name: "argument",
+            description: "Argument(s) the selected mode requires. Run command without this parameter to display help for the selected mode",
+            type: "string",
+            isOptional: true, // Optional to display help for selected mode if no argument is provided
+            ownersOnly: true
+        }
+    ],
+    ownersOnly: true,
+
+    /**
+     * The manage command
+     * @param {CommandHandler} commandHandler The commandHandler object
+     * @param {Array} args Array of arguments that will be passed to the command
+     * @param {function(object, object, string): void} respondModule Function that will be called to respond to the user's request. Passes context, resInfo and txt as parameters.
+     * @param {object} context The context (this.) of the object calling this command. Will be passed to respondModule() as first parameter.
+     * @param {CommandHandler.resInfo} resInfo Object containing additional information your respondModule might need to process the response (for example the userID who executed the command).
+     */
+    run: async (commandHandler, args, respondModule, context, resInfo) => {
+        const respond = ((txt) => respondModule(context, resInfo, txt)); // Shorten each call
+
+        const cmdusage = `'${resInfo.cmdprefix}manage mode argument'`; // TODO: Can't we dynamically generate this in the future?
+
+        // Prevent error when calling toLowerCase below when user did not provide any mode
+        if (!args[0]) args[0] = "";
+
+        switch (args[0].toLowerCase()) {
+            case "addaccount": { // Block "fixes" eslint no-case-declarations warning
+                // Check if no or too short (= probably incorrect format) credentials were provided
+                if (!args[1] || args[1].split(":").length < 2) {
+                    respond(await commandHandler.data.getLang("managecmdaddusage", { "credentialsformat": "username:password:sharedSecret", "optionalparameter": "sharedSecret", "cmdusage": cmdusage }, resInfo.userID));
+                }
+
+                // Split credentials into username, password and sharedSecret
+                const credentials = args[1].split(":");
+
+                // Run manage function with the provided credentials
+                commandHandler.controller.addAccount(credentials[0], credentials[1], credentials[2] || "");
+
+                respond(await commandHandler.data.getLang("managecmdaddsuccess", { "username": credentials[0] }, resInfo.userID));
+                break;
+            }
+
+            case "removeaccount":
+                // Check if no username was provided
+                if (!args[1]) return respond(await commandHandler.data.getLang("managecmdremoveusage", { "cmdusage": cmdusage }, resInfo.userID));
+
+                // Run manage function with the provided username
+                commandHandler.controller.removeAccount(args[1]);
+
+                respond(await commandHandler.data.getLang("managecmdremovesuccess", { "username": args[1] }, resInfo.userID));
+                break;
+
+            case "filteraccounts": { // Block "fixes" eslint no-case-declarations warning
+                const availableFilters = Object.keys(commandHandler.controller.filters);
+                const filtersLowercase = availableFilters.map((e) => e.toLowerCase());
+
+                // Check if no filter or an invalid filter was provided
+                if (!args[1] || !filtersLowercase.includes(args[1].toLowerCase())) {
+                    respond(await commandHandler.data.getLang("managecmdfilterusage", { "availablefilters": availableFilters.join(", "), "cmdusage": cmdusage }, resInfo.userID));
+                    return;
+                }
+
+                // Find the selected filter while ignoring case
+                const selectedFilter = availableFilters.find((e) => e.toLowerCase() == args[1].toLowerCase());
+
+                // Run manage function with the selected filter
+                const filterResult = commandHandler.controller.filterAccounts(commandHandler.controller.filters[selectedFilter]);
+
+                respond(await commandHandler.data.getLang("managecmdfiltersuccess", { "matchamount": filterResult.length, "selectedfilter": selectedFilter, "usernames": filterResult.map((e) => e.accountName).join(", ") }));
+                break;
+            }
+
+            default:
+                respond(await commandHandler.data.getLang("managecmdusage", { "availablemodes": availableManageModes, "cmdusage": cmdusage }, resInfo.userID));
         }
     }
 };
